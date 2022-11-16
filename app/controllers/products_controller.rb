767a7b9d7095b3e3be673e3054af2f15d832cce8
RyunosukeShinjo <2011140017p@ed.fuk.kindai.ac.jp>
2022-11-16 07:29:06 +0000

    controllerの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+    def new
+        @product = Product.new
+    end
+    def create
+        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], thumnail: params[:product][:thumnail])
+        @product.save
+        redirect_to '/'
+    end
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        redirect_to products_path
+    end
 end
