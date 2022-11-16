c17cec59ded6d1564b9e03c611c3dba5d3329195
RyunosukeShinjo <2011140017p@ed.fuk.kindai.ac.jp>
2022-11-16 07:20:58 +0000

    ルートの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,18 @@
 class ProductsController < ApplicationController
+    def index
+        @products = Product.all
+    end
+    def new
+        @product = Product.new
+    end
+    def create
+        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], thumbnail: params[:product][:thumbnail])
+        @product.save
+        redirect_to '/'
+    end
+    def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        redirect_to products_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
     post 'top/login'
     get 'top/logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
     resources :users
->>>>>>> 3ff5a47ad7df146d2c66143902ab73d29fe4acc3
 end
