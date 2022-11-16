ebd945a26449d6dc2e45e0ad287761de6edbe6ef
RyunosukeShinjo <2011140017p@ed.fuk.kindai.ac.jp>
2022-11-16 08:55:59 +0000

    productのindex,new,showがほぼ完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,8 @@ class ProductsController < ApplicationController
         @product = Product.new
     end
     def create
-        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], thumnail: params[:product][:thumnail])
+        image = params[:product][:image].read
+        @product = Product.new(title: params[:product][:title], price: params[:product][:price], description: params[:product][:description], image: image)
         @product.save
         redirect_to '/'
     end
@@ -15,4 +16,12 @@ class ProductsController < ApplicationController
         product.destroy
         redirect_to products_path
     end
+    def show
+        @product = Product.find(params[:id])
+    end
+    def get_image
+        product = Product.find(params[:id])
+        send_data product.image, disposition: :inline, type: 'image/png'
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
     post 'top/login'
     get 'top/logout'
+    
+    resources :products
+    root 'products#index'
+    get 'get_image/:id', to: 'products#get_image'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
