e5c640d9458df570077ab0f752125d5da4bbed6c
Ubuntu <ubuntu@ip-172-31-49-227.ec2.internal>
2023-01-16 13:18:35 +0000

    last

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     
     validates :name, :password, :address, presence: true 
-    validates :number, format: { with: /\A[0][5678]0[0-9]{8}/ }, presence: true, uniqueness: true
-    validates :email, format: { with: /\A[0-9]{10}+[a-z]{1}+@+ed.fuk.kindai.ac.jp/ }, uniqueness: true, presence: true
+    validates :number, format: { with: /\A[0][5678]0[0-9]{8}/ }, presence: true
+    validates :email, format: { with: /\A[0-9]{10}+[a-z]{1}+@+ed.fuk.kindai.ac.jp/ }, presence: true
 end
